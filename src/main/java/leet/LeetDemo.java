713f98edca5ffd4dcbeaba46ef2be41e774ca4eb
zhangjinchun <zhangjinchun@meituan.com>
2020-06-17 16:23:06 +0800

    update

---
+++ src/main/java/leet/LeetDemo.java
@@ -0,0 +1,34 @@
+package leet;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @author zhangjinchun
+ * @date 2020/6/16
+ */
+public class LeetDemo {
+    public LeetDemo(int k, int[] nums) {
+
+    }
+
+//    public int add(int val) {
+//
+//    }
+
+    public static void main(String[] args) {
+        String s = "/a//b////c/d//././/..";
+//        String result = simplifyPath(s);
+//        System.out.println(new Gson().toJson(result));
+    }
+}
