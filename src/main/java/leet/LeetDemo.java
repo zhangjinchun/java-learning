a28f00fd84e4dbc40116a95caac89412dfc57836
zhangjinchun <zhangjinchun@meituan.com>
2020-06-29 17:25:30 +0800

    update

--- src/main/java/leet/LeetDemo.java
+++ src/main/java/leet/LeetDemo.java
@@ -3,32 +3,36 @@ package leet;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
 import org.apache.commons.lang.StringUtils;
+import serializable.People;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 
 /**
  * @author zhangjinchun
  * @date 2020/6/16
  */
 public class LeetDemo {
-    public LeetDemo(int k, int[] nums) {
 
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        return new int[3];
     }
 
-//    public int add(int val) {
-//
-//    }
 
     public static void main(String[] args) {
-        String s = "/a//b////c/d//././/..";
-//        String result = simplifyPath(s);
-//        System.out.println(new Gson().toJson(result));
+        int[] s = {4,2,8,8,3,3,2};
+        int[] t = {8,2,0,9};
+        int[] result = intersection(s, t);
+        System.out.println(new Gson().toJson(result));
     }
 }
