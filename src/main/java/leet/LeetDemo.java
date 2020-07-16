7c238343c444ecb4b63c59893b7a05ad8f6c1821
zhangjinchun <zhangjinchun@meituan.com>
2020-07-16 14:15:21 +0800

    update

--- src/main/java/leet/LeetDemo.java
+++ src/main/java/leet/LeetDemo.java
@@ -2,14 +2,18 @@ package leet;
 
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
+import org.apache.commons.lang.CharUtils;
 import org.apache.commons.lang.StringUtils;
 import serializable.People;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -24,15 +28,58 @@ import java.util.TreeMap;
  */
 public class LeetDemo {
 
-    public static int[] intersection(int[] nums1, int[] nums2) {
-        return new int[3];
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static int numDecodings(String s) {
+        return getAns(s, 0);
+    }
+
+    private static int getAns(String s, int start) {
+        //划分到了最后返回 1
+        if (start == s.length()) {
+            return 1;
+        }
+        //开头是 0,0 不对应任何字母，直接返回 0
+        if (s.charAt(start) == '0') {
+            return 0;
+        }
+        //得到第一种的划分的解码方式
+        int ans1 = getAns(s, start + 1);
+        int ans2 = 0;
+        //判断前两个数字是不是小于等于 26 的
+        if (start < s.length() - 1) {
+            int ten = (s.charAt(start) - '0') * 10;
+            int one = s.charAt(start + 1) - '0';
+            if (ten + one <= 26) {
+                ans2 = getAns(s, start + 2);
+            }
+        }
+        return ans1 + ans2;
     }
 
 
     public static void main(String[] args) {
-        int[] s = {4,2,8,8,3,3,2};
-        int[] t = {8,2,0,9};
-        int[] result = intersection(s, t);
+        int[] nums = {-5};
+        String s = "102627";
+        String p = "dss";
+        String target = "VIV";
+
+        int result = numDecodings(s);
         System.out.println(new Gson().toJson(result));
     }
 }
