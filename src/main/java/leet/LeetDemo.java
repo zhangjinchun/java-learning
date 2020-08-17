9863f5db11502f2a6a869bb96db11c32038e8557
zhangjinchun <zhangjinchun@meituan.com>
2020-08-17 10:10:25 +0800

    update

--- src/main/java/leet/LeetDemo.java
+++ src/main/java/leet/LeetDemo.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang.CharUtils;
 import org.apache.commons.lang.StringUtils;
 import serializable.People;
 
+import javax.management.StandardEmitterMBean;
+import javax.swing.tree.TreeNode;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,6 +47,25 @@ public class LeetDemo {
         }
     }
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public static int numDecodings(String s) {
         return getAns(s, 0);
     }
@@ -72,14 +93,63 @@ public class LeetDemo {
         return ans1 + ans2;
     }
 
+    public static void heapSort(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return;
+        }
+        for (int i = (nums.length - 1) / 2; i >= 0; i--) {
+            adjustHeap(nums, i, nums.length - 1);
+        }
+        for (int j = nums.length - 1; j > 0; j--) {
+            int temp = nums[0];
+            nums[0] = nums[j];
+            nums[j] = temp;
+            adjustHeap(nums, 0, j - 1);
+        }
+    }
+
+    private static void adjustHeap(int[] nums, int parent, int len) {
+        int temp = nums[parent];
+        int lChild = parent * 2 + 1;
+        while (lChild <= len) {
+            int rChild = lChild + 1;
+            if (rChild <= len && nums[rChild] > nums[lChild]) {
+                lChild++;
+            }
+            if (temp < nums[lChild]) {
+                nums[parent] = nums[lChild];
+            }
+            parent = lChild;
+            lChild = parent * 2 + 1;
+        }
+        nums[parent] = temp;
+    }
+
+    public static void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode tempRight = root.right;
+        root.right = root.left;
+        flatten(root.left);
+        root = root.right;
+        flatten(tempRight);
+
+
+    }
+
 
     public static void main(String[] args) {
-        int[] nums = {-5};
+        TreeNode t1 = new TreeNode(2);
+        TreeNode t2 = new TreeNode(5);
+        TreeNode root = new TreeNode(1, t1,t2);
+        flatten(root);
+        int[] nums = {5, 9, 8, 3, 2, 6, 8, 5, 6, 6, 9};
         String s = "102627";
         String p = "dss";
         String target = "VIV";
 
-        int result = numDecodings(s);
-        System.out.println(new Gson().toJson(result));
+        heapSort(nums);
+        System.out.println(new Gson().toJson(nums));
     }
 }
