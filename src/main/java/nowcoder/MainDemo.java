a1edbff196a98e8bbf5c794770c5421f52fcd6ec
zhangjinchun <zhangjinchun@meituan.com>
2021-02-24 22:13:41 +0800

    update

--- src/main/java/nowcoder/MainDemo.java
+++ src/main/java/nowcoder/MainDemo.java
@@ -11,17 +11,33 @@ public class MainDemo {
         //Scanner in = new Scanner(System.in);
         //int a = in.nextInt();
         //System.out.println(a);
+
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(str);
+        String[] strArr = str.split(" ");
+        System.out.println(Arrays.toString(strArr));
+        int[] request = new int[strArr.length];
+        for (Integer i=0; i< request.length;i++) {
+            request[i] = Integer.parseInt(strArr[i]);
+        }
+        System.out.println(Arrays.toString(request));
+        int resust = getMinNotInArray(request);
+        System.out.println(resust);
         System.out.println("Hello World!");
     }
 
-    public int getMinNotInArray(int[] request) {
+    public static int getMinNotInArray(int[] request) {
         if (request == null) {
             return -1;
         }
         int len = request.length;
         int min = request[0];
-        for (int left=0; left < len; len++) {
-            for (int i=1; i< len; i++) {
+        if (len == 1) {
+            return request[0] == 1 ? 2 : 1;
+        }
+        for (int left=0; left < len -1; left++) {
+            for (int i=left +1; i< len-1; i++) {
                 if (request[i] < min) {
                     min = request[i];
                     int temp = request[left];
@@ -32,7 +48,7 @@ public class MainDemo {
             if (left == 0 && request[left]>1) {
                 return 1;
             }
-            if (min > request[left] + 1) {
+            if (request[left+1] > request[left] + 1) {
                 return request[left] + 1;
             }
         }
