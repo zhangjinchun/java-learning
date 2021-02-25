e944720147e5d03c3f7d2465de2b13253be73f4d
zhangjinchun <zhangjinchun@meituan.com>
2021-02-25 11:46:58 +0800

    update

--- src/main/java/nowcoder/MainDemo.java
+++ src/main/java/nowcoder/MainDemo.java
@@ -12,8 +12,9 @@ public class MainDemo {
         //int a = in.nextInt();
         //System.out.println(a);
 
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
+        // 读取一维整数数组输入
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         if (str == null || str.length() == 0 || str.equals("input is null")) {
             System.out.println(-1);
             return;
@@ -22,9 +23,10 @@ public class MainDemo {
         String[] strArr = str.split(" ");
         System.out.println(Arrays.toString(strArr));
         int[] request = new int[strArr.length];
-        for (Integer i=0; i< request.length;i++) {
+        for (Integer i = 0; i < request.length; i++) {
             request[i] = Integer.parseInt(strArr[i]);
         }
+
         System.out.println(Arrays.toString(request));
         int resust = getMinNotInArray(request);
         System.out.println(resust);
@@ -40,8 +42,8 @@ public class MainDemo {
         if (len == 1) {
             return request[0] == 1 ? 2 : 1;
         }
-        for (int left=0; left < len -1; left++) {
-            for (int i=left + 1; i< len-1; i++) {
+        for (int left = 0; left < len - 1; left++) {
+            for (int i = left + 1; i < len - 1; i++) {
                 if (request[i] < request[left]) {
                     int temp = request[left];
                     request[left] = request[i];
@@ -49,14 +51,14 @@ public class MainDemo {
                 }
             }
             if (left == 0) {
-                if (request[left]>1) {
+                if (request[left] > 1) {
                     return 1;
                 }
-            } else if (request[left] > request[left-1] + 1 && request[left] > 1) {
-                return Math.max(request[left-1] + 1, 1);
+            } else if (request[left] > request[left - 1] + 1 && request[left] > 1) {
+                return Math.max(request[left - 1] + 1, 1);
             }
         }
 
-        return request[len-1] +1;
+        return request[len - 1] + 1;
     }
 }
